9ec48fc2e7283bd3171a0a1f6600e82cae22383f
Arya Mane <aryamane1210@gmail.com>
2025-07-21 19:29:09 +0530

    Components folder created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function App() {
+  return (
+    <div>
+      <h1>Hello, React! 🚀</h1>
+      <p>Welcome to your first component.</p>
+    </div>
+  );
+}
+
+export default App;
